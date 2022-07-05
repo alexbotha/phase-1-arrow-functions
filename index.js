09546bceaea4df89d0b539ae822f42f48b05a628
alexbotha <alexbotha@hotmail.co.uk>
2022-07-05 19:04:47 +0100

    commit tree 16229ec839bb568504e218fad4b51ba8ace6985c

---
+++ index.js
@@ -0,0 +1,20 @@
+const divide = function(){
+  let divide = 2000
+  return (divide / 100)
+}
+
+const square = square => square + square
+
+
+const add = (para1,para2) => para1 + para2; {
+}
+
+
+
+  
+
+
+ 
+
+
+  
